1f663634758fba5b1e5a702582d25e57e9e61ad7
tessatiu <gadjiew.rasim@gmail.com>
2023-10-01 09:37:17 +0300

    receiver

---
+++ cmake-build-debug/CMakeFiles/IPC_MSG_RECEIVER.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IPC_MSG_RECEIVER.dir/main.cpp.o"
+  "IPC_MSG_RECEIVER"
+  "IPC_MSG_RECEIVER.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IPC_MSG_RECEIVER.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
